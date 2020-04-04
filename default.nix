684c4b53e84e7abbfea59acd1fd154ba41f5de8f
Robert Pearce <me@robertwpearce.com>
2020-04-04 17:15:45 -0400

    rm cruft

--- default.nix
+++ default.nix
@@ -11,14 +11,7 @@ pkgs: attrs:
       system = builtins.currentSystem;
     };
 
-    combinedAttrs = (defaultAttrs // attrs // (with pkgs.stdenv.lib; {
-      #version = "0.1.0.0";
-      #description = "⚙️  Use nix to manage your CLI tools, configuration dotfiles, and more";
-      #homepage = "https://github.com/rpearce/mgmt";
-      #license = licenses.bsd3.fullName;
-      #maintainers = with maintainers; [ rpearce ];
-      #platforms = platforms.unix;
-    }));
+    combinedAttrs = (defaultAttrs // attrs);
 
     finalAttrs = (combinedAttrs // {
       dotfiles = lib.attrsets.mapAttrsToList
